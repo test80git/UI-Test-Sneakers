cb83d71089d2a7f235321494236a6939bb0be1eb
test80git <test80git@yandex.ru>
2026-03-29 01:17:43 +0300

    init commit

---
+++ src/main/java/ru/uitestsneakerstore/UiTestSneakerStoreApplication.java
@@ -0,0 +1,13 @@
+package ru.uitestsneakerstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UiTestSneakerStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UiTestSneakerStoreApplication.class, args);
+    }
+
+}
